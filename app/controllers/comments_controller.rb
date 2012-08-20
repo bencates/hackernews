393c749ef94ce29417dd40f7459d67fde5d6a7b8
Ben Cates <ben.cates@gmail.com>
2012-08-20 17:00:21 -0400

    Nested comments working and displaying properly

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,16 +12,20 @@ class CommentsController < ApplicationController
   # POST /1/comments
   # POST /1/comments.json
   def create
-    @story = Story.find(params[:story_id])
-    @comment = @story.comments.build(params[:comment])
+    if params[:comment_id]
+      @parent = Comment.find(params[:comment_id])
+    else
+      @parent = Story.find(params[:story_id])
+    end
+    @comment = @parent.comments.build(params[:comment])
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @story, notice: 'Comment was successfully created.' }
-        format.json { render json: @story, status: :created, location: @story }
+        format.html { redirect_to @parent, notice: 'Comment was successfully created.' }
+        format.json { render json: @parent, status: :created, location: @parent }
       else
-        format.html { redirect_to @story, alert: 'Comment was not created.' }
-        format.json { render json: @story.errors, status: :unprocessable_entity }
+        format.html { redirect_to @parent, alert: 'Comment was not created.' }
+        format.json { render json: @parent.errors, status: :unprocessable_entity }
       end
     end
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   include Karma
 
   belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
   attr_accessible :body
 
   validates_presence_of :body
